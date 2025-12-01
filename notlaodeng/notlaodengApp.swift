53ec0a7dd6bb19ec2bd900df8fb38f576edf79e5
thomasy <c6swift@gmail.com>
2025-12-01 19:28:36 +0800

    Initial Commit

---
+++ notlaodeng/notlaodengApp.swift
@@ -0,0 +1,17 @@
+//
+//  notlaodengApp.swift
+//  notlaodeng
+//
+//  Created by yk on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct notlaodengApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
